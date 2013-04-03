a90aaadd41ae826db3882ae874d30cd7862a8fb1
XhmikosR <xhmikosr@users.sourceforge.net>
2013-04-03 11:28:28 +0300

    Put google analytics in <head>. It's asynchronous anyway.

--- source/_static/js/plugins.js
+++ source/_static/js/plugins.js
@@ -28,18 +28,3 @@
 $('#binaries, #pdb-files').find('.external').each(function() {
     $(this).addClass("sourceforge_accelerator_link");
 });
-
-// Google analytics
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-9335012-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
